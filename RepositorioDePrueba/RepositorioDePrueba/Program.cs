c0e9a131b19407638837f5f317c4cabeb109ecf3
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2022-10-08 18:59:33 -0700

    UPDATE terminamos el programa, pero sin ciclo
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

--- RepositorioDePrueba/RepositorioDePrueba/Program.cs
+++ RepositorioDePrueba/RepositorioDePrueba/Program.cs
@@ -23,7 +23,44 @@ namespace RepositorioDePrueba
             Console.WriteLine("--------------------------------------");
             //leer la opcion
             opcion = Console.ReadLine();
+            //pedimos operandos
+            Console.WriteLine("Captura el operando 1");
+            op1 = double.Parse( Console.ReadLine() );
+            Console.WriteLine("Captura el operando 2");
+            op2 = double.Parse( Console.ReadLine() );
+            //procesar la opcion pàra saber que operacion hacver
+            if (opcion == "a")
+            {
+                res = op1 + op2;
+                operacion = "SUMA";
+            }
+            else if (opcion == "b")
+            {
+                res = op1 - op2;
+                operacion = "RESTA";
+            }
+            else if (opcion == "c")
+            {
+                res = op1 * op2;
+                operacion = "MULTIPLICACION";
+            }
+            else if (opcion == "d")
+            {
+                res = op1 % op2;
+                operacion = "MODULUS";
+            }
+            else if (opcion == "x")
+                Console.WriteLine("SALIDA--- adios :(...");
+            else {
+                Console.WriteLine(opcion+" ES UNA OPCION NO VÁLIDA");
+            }
 
+            /// imprimir el resultado
+            Console.WriteLine("La operacion que elegiste "+operacion+" con los números op1="+op1+", y op2="+op2+" = "+res);
+            //cuialquier tecla para trerminar
+            Console.WriteLine( "Presiona cualquier tecla para terminar");
+            Console.ReadKey();
         }
+                
     }
 }
