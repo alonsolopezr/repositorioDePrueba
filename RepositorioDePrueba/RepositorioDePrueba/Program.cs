20c10c6b1bfaa37c769f4f1bf8ac5f12d144bd0e
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2022-10-08 18:33:34 -0700

    ADD se creó proyecto de C# para Operaciones
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

---
+++ RepositorioDePrueba/RepositorioDePrueba/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RepositorioDePrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----Def Vars----------
+            double op1 = 0;
+            double op2 = 0;
+            double res = 0;
+            string opcion = "";
+            string operacion = "";
+            //-----MENU----------
+            Console.WriteLine("Bienvenido al menu de opciones de operaciones");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("a) Suma");
+            Console.WriteLine("b) Resta");
+            Console.WriteLine("c) Multi");
+            Console.WriteLine("d) Modulus");
+            Console.WriteLine("x) Salir");
+            Console.WriteLine("--------------------------------------");
+            //leer la opcion
+            opcion = Console.ReadLine();
+
+        }
+    }
+}
